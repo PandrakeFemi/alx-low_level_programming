0e630b170dcea8ae5e3cacb55d7fad6e28b0486f
PandrakeFemi <pandrakefemi@gmail>
2022-09-09 13:36:30 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print all possible combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 8; var++)
+	{
+		int ii;
+
+		for (ii = var + 1; ii < 9; ii++)
+		{
+			int jj;
+
+			for (jj = ii + 1; jj < 10; jj++)
+			{
+				putchar((var % 10) + '0');
+				putchar((ii % 10) + '0');
+				putchar((jj % 10) + '0');
+
+				if (var != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				} else
+					continue;
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
