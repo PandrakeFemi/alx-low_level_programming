fa5067697abc774e59949381dfdc473c908e88a6
PandrakeFemi <pandrakefemi@gmail>
2022-09-13 16:33:53 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
- * main - prints out sum of even values of a fibonacci
+ * main - Find and print the first 98 fibo numbers from 1 and 2
  *
  * Return: 0
  */
 int main(void)
 {
-	int i = 1, j = 2, total = 0;
-	int k;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
 
-	while (j < 4000000)
+	unsigned long int hold1, hold2, hold3;
+
+	int count;
+
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		if (j % 2 == 0)
-			total += j;
-
-		k = j;
-		j += i;
-		i = k;
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
-	printf("%d\n", total);
+	printf("\n");
 	return (0);
 }
