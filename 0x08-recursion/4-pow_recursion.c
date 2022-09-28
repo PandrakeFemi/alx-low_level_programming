a61dd7995e2c7d56d89b83e289a4731a86ee54fa
PandrakeFemi <pandrakefemi@gmail>
2022-09-28 12:56:21 -0700

    3-factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found*/
+
