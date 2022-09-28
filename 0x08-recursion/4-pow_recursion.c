b490cb78a6899a10db6f77ecb3b26b52a389d0fd
PandrakeFemi <pandrakefemi@gmail>
2022-09-28 13:00:30 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,5 +5,19 @@
  * @x: base
  * @y: exposant
  * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found*/
+ * or NULL if no such byte is found.
+ */
 
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+	return (-1);
+else if (y == 1)
+	return (x);
+else if (y == 0)
+	return (1);
+
+return (x * _pow_recursion(x, y - 1));
+
+}
