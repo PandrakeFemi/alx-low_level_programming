27189c0660024ad9b04937a2f0e40a5efc18ba93
PandrakeFemi <pandrakefemi@gmail>
2022-10-03 06:13:25 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,14 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - Frees a 2-dimensional array of integers.
- * @grid: The 2-dimensional array of integers to be freed.
- * @height: The height of grid.
+ * argstostr - Concatenates all arguments of the program into a string;
+ * arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ * Otherwise - a pointer to the new string.
  */
-void free_grid(int **grid, int height)
+char *argstostr(int ac, char **av)
 {
-int index;
-for (index = 0; index < height; index++)
-free(grid[index]);
-free(grid);
+char *str;
+int arg, byte, index, size = ac;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (arg = 0; arg < ac; arg++)
+{
+for (byte = 0; av[arg][byte]; byte++)
+size++;
+}
+str = malloc(sizeof(char) * size + 1);
+if (str == NULL)
+return (NULL);
+index = 0;
+for (arg = 0; arg < ac; arg++)
+{
+for (byte = 0; av[arg][byte]; byte++)
+str[index++] = av[arg][byte];
+str[index++] = '\n';
+}
+str[size] = '\0';
+return (str);
 }
