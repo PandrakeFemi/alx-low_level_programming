a3f8cfc09b52b9ab0b9f7cce439d5fbfc6276083
PandrakeFemi <pandrakefemi@gmail>
2022-10-10 17:21:54 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
+
 /**
- * main - Entry Point
- * Return: 0
+ * main - Entry point, prints file name it was compiled from
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
