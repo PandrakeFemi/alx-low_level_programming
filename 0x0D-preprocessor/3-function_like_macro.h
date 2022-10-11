a2fd4ea1340c9c836d045f6a9eb1d290a1de7430
PandrakeFemi <pandrakefemi@gmail>
2022-10-10 17:25:46 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-
-#endif /* FUNCTION_LIKE_MACRO */
+#ifndef ABS_H
+#define ABS_H
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
