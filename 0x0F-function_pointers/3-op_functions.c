f8b1ec8ea98d3d786dc2db31372fb3a55580a408
PandrakeFemi <pandrakefemi@gmail>
2022-10-12 14:16:39 -0700

    Update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -58,4 +58,33 @@ int op_mul(int a, int b)
  */
 
 int op_div(int a, int b)
-	
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - Calculates the remainder of @a divided by @b
+ *
+ * @a: A number
+ *
+ * @b: A number
+ *
+ * Return: Remainder of @a divider by @b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
